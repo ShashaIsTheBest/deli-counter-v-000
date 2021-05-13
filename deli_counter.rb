f1cffb6b5c75b7c3b12d2b9ec3855f1311b3bf06
Shasha Yan <shashayan@gmail.com>
2021-05-13 18:04:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,8 +6,8 @@ def line(katz_deli)
   else
     current_line = "The line is currently:"
     katz_deli.each.with_index(1) do |person, i|
-      current_line << " #{i}.#{person}"
-    end
+      current_line << " #{i}. #{person}"
+  end
     puts current_line
   end
 end
